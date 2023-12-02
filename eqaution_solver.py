80b3febdee3b73ba5f56ebb1d05a0ffd50a44626
SoumenX-CZ <90548394+SoumenX-CZ@users.noreply.github.com>
2023-12-02 20:48:39 +0100

    Update eqaution_solver.py
    Comments added.

--- eqaution_solver.py
+++ eqaution_solver.py
@@ -2,6 +2,8 @@ from sympy.solvers import solve
 from sympy import Symbol
 
 x = Symbol("x")
+
+#Three equations
 print(solve(x ** 2 - 1, x))
 print(solve(x ** 3 + x ** 2 + x + 1, x))
 print(solve(x ** 3 - 0 * x ** 2 + 4 * x - 5, x))
